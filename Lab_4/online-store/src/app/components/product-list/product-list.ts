62c61508819e46ce8c2edbf94b0890b3c15fcce8
kussan000 <m_kussan@kbtu.kz>
2026-03-03 16:33:23 +0600

    .

---
+++ Lab_4/online-store/src/app/components/product-list/product-list.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCard } from '../product-card/product-card';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, ProductCard],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList {
+
+
+
+}
